0cc6fed2d6fd98059f8b358c8bf0e919467489d2
mathiasv2 <dumas.mathias@outlook.com>
2026-05-29 16:24:29 +0200

    fixing the fixing fix

---
+++ QuiSNCF/Migrations/20260529142403_SyncModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuiSNCF.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Players",
+                keyColumn: "PlayerId",
+                keyValue: 1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Players",
+                columns: new[] { "PlayerId", "Name", "Score", "Tries" },
+                values: new object[] { 1, "Caca", 100, 1 });
+        }
+    }
+}
