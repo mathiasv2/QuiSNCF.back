e1c851f5ca6d07636f9ce75e6d761e34827ab1c1
mathiasv2 <dumas.mathias@outlook.com>
2026-05-29 16:21:58 +0200

    fixed the fix

--- QuiSNCF/Seeds/word.sql
+++ QuiSNCF/Seeds/word.sql
@@ -1,4 +1,4 @@
 INSERT INTO "Words" ("WordName", "Definition", "LastTimePlayed") VALUES
-('Debords', 'Cour et voies où les wagons sont en chargement et déchargement par les clients.'),
-('Decauville','Chemin de fer à voie très étroite: 0,60 m. Etait très utilisé dans l''armée, sur les chantiers, dans les mines. Nombre de petits tacots touristiques actuels sont de ce type.'),
-('Ecartement', 'Sur la SNCF, l''écartement est dit normal, c''est-à-dire 1,435 m. Il existe d''autres écartement 1 m, 0,60, etc.');
+('Debords', 'Cour et voies où les wagons sont en chargement et déchargement par les clients.', '0001-01-01'),
+('Decauville', 'Chemin de fer à voie très étroite: 0,60 m. Etait très utilisé dans l''armée, sur les chantiers, dans les mines. Nombre de petits tacots touristiques actuels sont de ce type.', '0001-01-01'),
+('Ecartement', 'Sur la SNCF, l''écartement est dit normal, c''est-à-dire 1,435 m. Il existe d''autres écartement 1 m, 0,60, etc.', '0001-01-01');
