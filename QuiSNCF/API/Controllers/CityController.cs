cd37b9fb7953188f5469e4f11aac83388f4f6695
mathiasv2 <dumas.mathias@outlook.com>
2026-07-10 10:32:01 +0200

    feat: controller to get departures

--- QuiSNCF/API/Controllers/CityController.cs
+++ QuiSNCF/API/Controllers/CityController.cs
@@ -1,11 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using QuiSNCF.Repository;
+using QuiSNCF.Service;
 
 namespace QuiSNCF.API.Controllers;
 
 [Route("api/[controller]")]
-public class CityController(ICityRepository repo): ControllerBase
+public class CityController(SNCFApiRequest request): ControllerBase
 {
+    [HttpGet("{name}")]
+    public async Task<IActionResult> Get(string name)
+    {
+        var result = await request.GetCityId(name);
+        return Ok(result);
+    }
     
-    
+    [HttpGet("departures/{name}")]
+    public async Task<IActionResult> GetDepartures(string name)
+    {
+        var result = await request.GetNextDepartures(name);
+        return Ok(result);
+    }
 }
