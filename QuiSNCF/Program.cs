f90acb18da0810d0c181e3dd775793c4aac201ab
mathiasv2 <dumas.mathias@outlook.com>
2026-07-22 10:40:04 +0200

    dependancy injection for cityrepo

--- QuiSNCF/Program.cs
+++ QuiSNCF/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<IStationRepository, StationRepository>();
 builder.Services.AddScoped<IPlayerRepository, PlayerRepository>();
 builder.Services.AddScoped<IWordRepository, WordRepository>();
+builder.Services.AddScoped<ICityRepository, CityRepository>();
 builder.Services.AddScoped<DailyPickRepository>();
 builder.Services.AddScoped<SNCFApiRequest>();
 builder.Services.AddScoped<SNCFApiMapperToDepartureView>();
