5e587c555aeed32eb6660c7de57fdde10887a31d
mathiasv2 <dumas.mathias@outlook.com>
2026-07-29 18:37:26 +0200

    controller to test input

--- QuiSNCF/API/Controllers/CityController.cs
+++ QuiSNCF/API/Controllers/CityController.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
+using QuiSNCF.Models;
 using QuiSNCF.Repository;
+using QuiSNCF.Service;
 
 namespace QuiSNCF.API.Controllers;
 
 [Route("api/[controller]")]
-public class CityController(ICityRepository repo): ControllerBase
+public class CityController(DailyPickRepository picker): ControllerBase
 {
-    
-    
+    [HttpPost("checkinput/{input}")]
+    public async Task<IActionResult> CheckInput(string input)
+    {
+        bool correct = await picker.IsInputRight<City>(input);
+        var cityName = correct ? await picker.GetTodaysAnswer<City>() : null;
+        return Ok(new { correct, cityName });
+    }
 }
