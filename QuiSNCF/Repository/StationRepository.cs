2c825ccb40f6eea5a0fb4cffceed4602f607686b
mathiasv2 <dumas.mathias@outlook.com>
2026-08-03 13:27:11 +0200

    testing with zoom on middle

--- QuiSNCF/Repository/StationRepository.cs
+++ QuiSNCF/Repository/StationRepository.cs
@@ -68,8 +68,8 @@ public class StationRepository(GameDbContext db, ILogger<StationRepository> logg
     private (double x, double y) TodayRandomZoom()
     {
         Random rdn = new Random();
-        double x = rdn.NextDouble();
-        double y = 0.3 + rdn.NextDouble() * 0.7;
+        double x = 0.5;
+        double y = 0.5;
         
         logger.Success($"Zoom du jour : {x},{y}");
         return (x, y);
